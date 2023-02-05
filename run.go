29a901c3b7a3361f17ea583642c6804d238162b3
Kahaan H Patel <pate1410@eceprog4.ecn.purdue.edu>
2023-02-05 17:09:21 -0500

    Change in functions

--- run.go
+++ run.go
@@ -7,7 +7,22 @@ import (
     "regexp"
 )
 
-func readInput(inputFile string){
+func seperateLinks(links[] string) ([]string,[]string){
+    var re = regexp.MustCompile(`(?m)github`)
+
+    var github[]string
+    var npm[]string
+    for _, line := range links {
+        if re.MatchString(line){
+            github = append(github,line)
+        }else{
+            npm = append(npm,line)
+        }
+    }
+    return github,npm
+}
+
+func readInput(inputFile string)[]string{
     readfile,err := os.Open(inputFile)
 
     if err != nil {
@@ -25,27 +40,14 @@ func readInput(inputFile string){
     }
     readfile.Close()
 
-    var re = regexp.MustCompile(`(?m)github`)
-
-    var github[]string
-    var npm[]string
-    for _, line := range fileLines {
-        // Testing if we are able to print correct output from input file
-        // fmt.Println(line)
-
-        // Seperating github and npm in to their respecting arrays
-        if re.MatchString(line){
-            github = append(github,line)
-        }else{
-            npm = append(npm,line)
-        }
-    }
-
-
+    return fileLines
 }
+
 
 func main(){
     inputFile := os.Args[1]
-    fmt.Println(inputFile)
-    readInput(inputFile)
+    links:=readInput(inputFile)
+    githubLinks,npmLinks := seperateLinks(links)
+    fmt.Println(githubLinks)
+    fmt.Println(npmLinks)
 }
