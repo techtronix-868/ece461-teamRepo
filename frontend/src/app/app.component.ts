e92a62552c846ded3ac8059bc5d0844e4af5f607
Mark <mbaumste@purdue.edu>
2023-03-22 20:12:39 -0400

    Swagger Hub generated frontend service

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { DefaultService, AuthenticationRequest, User, UserAuthenticationInfo} from 'generated';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [DefaultService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend';
+
+  constructor(private service: DefaultService) {
+
+  }
+
+  ngOnInit(): void {
+    var user:User = {name: "mabaums", isAdmin: true};
+    var authInfo:UserAuthenticationInfo = {password: "mabaums"};
+    var request:AuthenticationRequest = {user: user, secret: authInfo};
+
+    this.service.createAuthToken(request).subscribe(body=> {
+      console.log(body);
+    });
+  }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,7 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BASE_PATH } from 'generated';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,9 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [{ provide: BASE_PATH, useValue: "http://localhost:8000"}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
