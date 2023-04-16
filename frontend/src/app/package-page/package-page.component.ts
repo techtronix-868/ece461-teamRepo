18d293ab9c29a788b142e536c203541bca4fcaa5
Mark <mbaumste@purdue.edu>
2023-04-16 12:11:39 -0400

    Displaying raw data from backend

--- frontend/src/app/package-page/package-page.component.ts
+++ frontend/src/app/package-page/package-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { DefaultService, PackageData, PackageHistoryEntry } from 'generated';
+import { DefaultService, PackageData, PackageHistoryEntry, PackageRating } from 'generated';
 
 @Component({
   selector: 'app-package-page',
@@ -14,18 +14,28 @@ export class PackagePageComponent implements OnInit {
   name: string = ""
   id: string = ""
   pkg_history: PackageHistoryEntry[]
+  pkg_rate: PackageRating
+
   ngOnInit(): void {
     this.route.queryParams.subscribe( params => {
       this.name = params['name'];
       this.id = params['id'];
       this.getPackageByName()
+      this.ratePackage()
     })
   }
 
   getPackageByName() {
     this.service.packageByNameGet(this.name, "").subscribe(body => {
       this.pkg_history = body;
-      console.log("Retrieved pkg ", this.name, " ", this.pkg_history)
+      console.log("Retrieved pkg ", this.name, " ", this.pkg_history);
+    })
+  }
+
+  ratePackage() {
+    this.service.packageRate(this.id, "").subscribe(body => {
+      this.pkg_rate = body;
+      console.log("Recieved rating: ", this.pkg_rate)
     })
   }
 }
